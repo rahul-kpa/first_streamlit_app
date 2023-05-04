57558d98f28caa4b26a344ef746f83a1e7408490
rahul-kpa <132610444+rahul-kpa@users.noreply.github.com>
2023-05-04 15:55:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('Hi, I am Rahul , learing new stuff')
 
-streamlit.Header('Hi, I am also Rubai')
+streamlit.header('Hi, I am also Rubai')
 
-streamlit.Text('have sound knoledge Azure')
-streamlit.Text('Power BI & Tableau')
-streamlit.Text('Snowflake')
+streamlit.text('have sound knoledge Azure')
+streamlit.text('Power BI & Tableau')
+streamlit.text('Snowflake')
